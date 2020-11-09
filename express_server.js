a0eed56239db457410966a302d49671646a447ff
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-09 19:37:30 +0000

    routes added

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ const urlDatabase = {
 app.get('/', (req,res) => {
   res.send('Hello!');
 });
+app.get('/urls.json',(req,res)=>{
+res.json(urlDatabase);
+})
 
 app.listen(port,()=>{
   console.log(`server is running on port ${port}`)
